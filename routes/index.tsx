9535fc8e076f2012179856bdc8895ea138111e7f
sirvess <436755-Sirvess@users.noreply.gitlab.com>
2022-09-05 08:51:09 +0100

    add gitignore

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,21 +1,22 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
+import { Button } from "../components/Button.tsx";
+import Derpy from "../islands/CitySearchBar.tsx";
 
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+      <form>
+        <input
+          type="text"
+          class={tw`border-black border-solid border-2 p-6`}
+          placeholder="Enter your location"
+          required
+        >
+        </input>
+      </form>
+      <Derpy />
     </div>
   );
 }
--- utils/twind.ts
+++ utils/twind.ts
@@ -1,8 +1,21 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Configuration, setup } from "twind";
+import * as colors from "twind/colors";
+
 export * from "twind";
 export const config: Configuration = {
-  darkMode: "class",
-  mode: "silent",
+  theme: {
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      yellow: colors.yellow,
+      green: colors.green,
+      blue: colors.blue,
+      indigo: colors.indigo,
+    },
+  },
 };
+
 if (IS_BROWSER) setup(config);
